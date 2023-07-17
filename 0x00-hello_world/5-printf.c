32d73e6d0d1a89b1831b416b1628ea5bec1e12a4
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-07-17 01:19:22 -0700

    print it out

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - main body
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
