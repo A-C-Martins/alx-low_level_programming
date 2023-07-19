6d04a84b7ae8c377ca6a41a00871834279b997f0
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-07-19 04:27:01 -0700

    initial commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -24,13 +24,12 @@ int print_sign(int n)
 		if (n == 0)
 		{
 			putchar('0');
-			return (0);
+			return((0);
 		}
 		else
 		{
 			putchar('-');
 			return (-1);
 		}
-	putchar('\n');
 	return (0);
 }
