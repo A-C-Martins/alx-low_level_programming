9c9d5cfb8f836a91bfcd4a62c0f9d7185760d22a
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-07-19 16:44:38 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,26 +14,20 @@
 
 int print_sign(int n)
 {
-	int size = 100;
-
-	while (n <= size)
+	if (n > 0)
 	{
-		if (n > 0)
+		_putchar('+');
+		return (1);
+	}
+	else
+		if (n == 0)
 		{
-			putchar('+');
-			return (1);
+			_putchar('0');
+			return (0);
 		}
 		else
-			if (n == 0)
-			{
-				putchar('0');
-				return (0);
-			}
-			else
-			{
-				putchar('-');
-				return (-1);
-			}
-	}
-	return (0);
+		{
+			_putchar('-');
+			return (-1);
+		}
 }
