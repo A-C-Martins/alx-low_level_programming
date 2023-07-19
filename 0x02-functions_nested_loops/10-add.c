b04ff943600cd45da5382738b1c8833e372832f4
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-07-19 05:02:41 -0700

    sum theapple

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - sums two number
+ * @int: the operands
+ *
+ * Return: sum
+ */
+int add(int, int)
+{
+	int i, j, sum;
+
+	sum = i + j;
+
+	return (sum);
+}
