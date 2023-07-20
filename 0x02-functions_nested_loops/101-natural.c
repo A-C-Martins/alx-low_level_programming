0f089b1d6c9b9bd835990a42487292da8a9bff46
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-07-19 19:47:21 -0700

    it is so quantigeous

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stddio.h>
+#include "main.h"
+
+/**
+ * main - prints the sum of multiples of 5 and 3
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int digit, sum = 0;
+
+	for (digit = 0; digit < 1024; digit++)
+	{
+		if ((digit % 3) == 0 || (digit % 5) == 0)
+			sum += digit;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
