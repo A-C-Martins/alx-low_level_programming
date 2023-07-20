ae771a6df9578080f6c37f526c8522455aa99653
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-07-19 19:18:40 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * print_to_98 - prints a natural number,
+ * seperated by comma
+ *
+ * @n: number count
+ */
+
 void print_to_98(int n)
 {
 	int size = 98;
 
-	for (n = 0; n <= size; n++)
+	if (n >= size)
 	{
-		_putchar(n + '0');
-		if (n > size)
+		for (n = 0; n > size; n--)
 		{
-			for (n = 98; n >= size; n--)
-			{
-				putchar(n + '0');
-			}
+			printf("%d, ", n - 1);
 		}
+		printf("%d\n", n);
+	}
+	else
+	{
+		for (n = 0; n < size; n++)
+		{
+			printf("%d, ", n++);
+		}
+		printf("%d\n", n);
+	}
+}
