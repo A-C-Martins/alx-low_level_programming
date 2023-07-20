de03cb2590ee4477ef42d074197a47282b13583a
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-07-19 17:41:42 -0700

    I am agent Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,12 +9,11 @@ void jack_bauer(void)
 {
 	int hour = 0;
 	int minute = 0;
-       
+	
 	for (hour = 0; hour <= 23; hour++)
 	{
 		for (minute = 0; minute <= 59; minute++)
 		{
-
 			printf("%02d:%02d\n", hour, minute);
 		}
 	}
