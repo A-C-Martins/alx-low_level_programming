f6e1dc63c99e5115bdcdd9a9e56d0c629272e9f6
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-07-20 18:28:04 -0700

    me vs me

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * peint_line - draws a horizontal line
+ * @n: counter
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i != 0 || i <= 0)
+		{
+			_putchar('-');
+		}
+	}
+	_putchar('\n');
+}
