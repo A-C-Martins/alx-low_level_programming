16cfbd2d981e1f0957db0ad6ed5670b23ad1cbea
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-07-20 17:15:29 -0700

    have you drinked multina before

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Multiply two operands
+ * @a: first operand
+ * @b: multiplying operand
+ *
+ * Return: multied
+ */
+
+int mul(int a, int b)
+{
+	int multied;
+
+	multied = a * b;
+	return (multied);
+}
