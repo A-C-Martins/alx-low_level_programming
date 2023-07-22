1ad65f284ad142e6c8587147f3cc603140d880c0
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-07-21 18:25:30 -0700

    Numbers have life; they are not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * print_number - returns a giving number
+ * @n: input output
+*/
 void print_number(int n)
 {
-	int des;
+	/* checks if number is negative if it is*/
 
-	des = n;
-	_putchar(des + '0');
+	if (n < 0)
+	{
+		/* prints '-' */
+		_putchar('-')
+
+		/* changes the value to absolute value */
+		n = -n;
+	}
+	/* checks if n is greater or equal to 10 */
+
+	if (n / 10)
+	{
+		/* function call it self to make sure the digit are printed correctly*/
+
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
 }
