5868ba95c61f84c94863025907f0b30c2f5ee3a2
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-07-24 21:30:49 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#includee "main.h"
+/**
+ * rev_string - reverses string
+ * @s: string to be reversed
+ *
+ * Author - AC Martins:
+ */
+void rev_string(char *s)
+{
+	int i;
+	char *tmp;
+
+	for (i = 0; s[c] != 0; i++)
+	{
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		tmp = s[i];
+	}
+}
