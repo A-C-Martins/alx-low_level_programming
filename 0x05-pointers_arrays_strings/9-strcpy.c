df45dddf0f88485f2f45692414932c50b7f0c20d
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-07-25 18:14:14 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcpy - copys the value of  a string
+ * @dest: Array i
+ * @src: Array 2
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, j;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j < i; j++)
+	{
+		dest[j] = src[i];
+	}
+
+	dest[j] = '\0';
+	return (dest);
+}
