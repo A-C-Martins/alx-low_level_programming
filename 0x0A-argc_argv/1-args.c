4bdfc79bc5b032c2726cdc15bfa0abe2fd286a35
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-08-04 18:28:30 -0700

    ilence is argument carried out by other means

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program start up
+ * @arcount: - Argument count
+ * @argv: Agument vector
+ *
+ * Return: 0 success
+ */
+
+int main(int arcount, char *argv[])
+{
+
+	printf("%d\n", arcount);
+
+	(void)argv;
+
+	return (0);
+}
