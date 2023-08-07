82d0d2f79ae07697e428f7fe232f4f3eb3a55012
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-08-07 14:01:31 -0700

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - Finds the length of a sring
+ * @s: The token which length is tobe found
+ *
+ * Return: i
+ */
+int _strlen(char *s)
+{
+    int i;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+
+    }
+    return i;
+}
+
+/**
+ * _strdup -  returns a pointer to a newly allocated space in memory, which
+ * contains a copy of the string given as a parameter.
+ * @str: String to be copied
+ *
+ * Return: strDup(the duplicated var
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int size;
+	char *strDup;
+
+	size = _strlen(str);
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+
+	strDup = (char *)malloc(size + 1);
+
+	if (strDup == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		strDup[i] = str[i];
+	}
+	return (strDup);
+}
