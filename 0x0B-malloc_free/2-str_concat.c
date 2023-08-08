9b62438b8b7210dbbbba42b4ca444d81e3418ed0
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-08-08 14:59:01 -0700

    If you even dream of beating me you do better wake up and apologize

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,6 +32,11 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 	int len1, len2, i = 0, j = 0;
 
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 
