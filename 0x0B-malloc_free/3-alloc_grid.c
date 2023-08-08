408c94b696d45affc91591e65e7a36976dbb494b
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-08-08 16:24:08 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -39,16 +39,10 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
-
-	if (modi_arrGrid == NULL)
-	{
-		return (NULL);
-	}
-
 	/* assigning value */
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < height; j++)
+		for (j = 0; j < width; j++)
 		{
 			modi_arrGrid[i][j] = 0;
 		}
