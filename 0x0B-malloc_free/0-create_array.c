203ea2f45abd27fd6a7e002af6eb94728a20bff1
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-08-07 19:04:24 -0700

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	buffer = (char *)malloc(size * (sizeof(unsigned int) + 1));
+	buffer = malloc(size * (sizeof(c)));
 
 	if (buffer == NULL)
 	{
