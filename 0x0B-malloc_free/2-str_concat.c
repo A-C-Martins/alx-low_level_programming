13c040b16a398ea414a71496996f4852dab1b130
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-08-08 14:49:50 -0700

    If you even dream of beating me you do better wake up and apologize

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strlen - Finds the length of a sring
+ * @s: The token which length is tobe found
+ *
+ * Return: i
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+
+/**
+ * str_concat - concatenats two string
+ * @s1: first string
+ * @s2: String to be merged
+ *
+ * Return: newly allocated pointer to array
+*/
+char *str_concat(char *s1, char *s2)
+{
+	char *str;
+	int len1, len2, i = 0, j = 0;
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	str = malloc(len1 + len2 + 1);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		str[i] = s1[i];
+	}
+
+	for (j = 0; (s1[i] + s2[j]) != '\0'; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
+
+	str[i] = '\0';
+
+	return (str);
+}
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid -  returns a pointer to a 2 dimensional array of integers.
+ * @width: represent the row
+ * @height: is for column
+ *
+ * Return: A pointer to 2d array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **modi_arrGrid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	modi_arrGrid = (int **)malloc(width * sizeof(int *));
+
+	for (i = 0; i < width; i++)
+	{
+		modi_arrGrid[i] = (int *)malloc(height * sizeof(int));
+	}
+
+	if (modi_arrGrid == NULL)
+	{
+		return (NULL);
+	}
+
+	/* assigning value */
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			modi_arrGrid[i][j] = 0;
+		}
+	}
+
+	return (modi_arrGrid);
+}
