8d951e4173dfdaf67853476ba272626bc47d0428
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-08-14 21:18:33 -0700

    SUM

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints corrent source code file name
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
