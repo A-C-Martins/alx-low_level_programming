1e7562ecf10180ca9d7273fbf476eb52cd92bad8
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-08-15 16:24:14 -0700

    It only take a pound symbol for you to be expanded by the preprocessor

---
+++ 0x0D-preprocessor/3-functionn_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTÍON_LIKE_MACRO_H
+
+#define ABS(x) ((x < 0) ? -(x) : (x))
+
+#endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,5 +1,5 @@
-#ifndef 4_SUM_H
-#define 4_SUM_H
+#ifndef SUM_H
+#define SUM_H
 
 #define SUM(x, y) ((x) + (y))
 
