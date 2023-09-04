9084c6d8ba8896f77ea9dd701c3e41a53cb762b1
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-09-04 16:41:45 -0700

    print thecode from a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,60 +1,37 @@
 #include "main.h"
 
 /**
- * read_textfile - Prints the content of a file
- * @filename: file to be accessed
- * @letters: number of strings to be printed
- * 
- * Return: letters
-*/
+ * read_textfile - Read a text file and print to POSIX stdout
+ * @filename: char string of files name
+ * @letters: number of letters to read and print
+ * Return: number of letters read and printed, or 0 if error
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fptr;
-	char *myString, *arr = ".txt", *myString1 = '\0';
-	size_t i, j = 0;
+	int fd;
+	ssize_t rcount, wcount;
+	char *buffer;
 
-	myString = malloc(sizeof(filename) + sizeof(arr));
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDWR);
 
-	if (myString == NULL)
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
+		free(buffer);
 		return (0);
 	}
-
-	for (i = 0; filename[i] != '\0'; i++)
-	{
-		myString[j] = filename[i];
-		j++;
-	}
-
-	if (filename[i] != ".")
-	{
-		for (i = 0; arr[i] != '\0'; i++)
-		{
-			myString[j] = arr[i];
-			j++;
-		}
-	}
-	myString[j] = '\0';
-
-	printf("%s\n", filename);
-
-	for (i = 0; myString[i]; i++)
-	{
-		fptr = fopen(myString, "r");
-	}
-
-	if (myString1[j] < letters || myString1[j] > letters)
-	{
+	rcount = read(fd, buffer, letters);
+	if (rcount == -1)
 		return (0);
-		j++;
-	}
-	else
-		fgets(myString1, letters, fptr);
+	wcount = write(STDOUT_FILENO, buffer, rcount);
+	if (wcount == -1 || rcount != wcount)
+		return (0);
 
-	printf("%s", myString1);
-
-	fclose(fptr);
-
-	free(myString);
-	return (letters);
+	free(buffer);
+	close(fd);
+	return (wcount);
 }
