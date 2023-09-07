ccdd2a1054cf8bd1efda97b16c66111ab3caf7be
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-09-06 20:00:55 -0700

    updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,22 +14,33 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDWR);
 
-	if (fd == -1)
-		return (0);
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
 		free(buffer);
 		return (0);
 	}
+
 	rcount = read(fd, buffer, letters);
 	if (rcount == -1)
+	{
+		free(buffer);
 		return (0);
+	}
+
 	wcount = write(STDOUT_FILENO, buffer, rcount);
 	if (wcount == -1 || rcount != wcount)
+	{
+		free(buffer);
 		return (0);
+	}
 
 	free(buffer);
 	close(fd);
