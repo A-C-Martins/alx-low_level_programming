0d0c38b91be8568cbeb320a5e15314e0359d70dc
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-10-26 18:27:01 -0700

    convert to unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts numbers into an unsigned int
+ * @b: chararcter string
+ * Return: converted decimal
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int all, pw;
+	int size;
+
+	if (b == NULL)
+	{
+		return(0);
+	}
+	for (size = 0; b[size]; size++)
+	{
+		if (b[size] != '0' && b[size] != '1')
+		{
+			return(0);
+		}
+	}
+
+	for (pw = 1, all = 0, size--; size >= 0; size-, pw *= 2)
+	{
+		if (b[size] == '1')
+		{
+			total +=  pw;
+		}
+	}
+
+	return(all);
+}
