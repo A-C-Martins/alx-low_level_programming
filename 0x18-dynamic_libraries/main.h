8ffd887c0e18efc2a36e47e5475ad6c2fd14cae5
A-C-Martins <arohchukwuebuka91@gmail.com>
2023-12-11 19:33:29 -0800

    *.cs

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -8,7 +9,7 @@ int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _puts(char *s);
+void _puts(char *str);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
